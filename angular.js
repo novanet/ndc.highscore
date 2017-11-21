fdca03025ecfa85410f859503f07211b0f9ace3c
hbrotan <hbrotan@gmail.com>
2017-11-21 07:50:27 +0100

    correct urls for api

--- angular.js
+++ angular.js
@@ -114,7 +114,7 @@
 		
 		function getLatestPhoto() {
             return $q(function(resolve, reject) {
-                $http.get('http://novanet-ndc-highscore.azurewebsites.net/api/lastPhoto')
+                $http.get('http://novanetndc2017highscore.azurewebsites.net/api/lastPhoto')
                     .then(function(response) {
                         resolve(response.data);
                     });
